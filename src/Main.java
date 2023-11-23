f397f882ad60385ec1250add75f942e715974741
Jonathan Samuel Tanadi <79454730+JonathanSamuelTan@users.noreply.github.com>
2023-11-23 08:51:32 +0700

    FIX : adjust main.java for merging

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
-import model.Database;
-import model.User;
 import view.LoginView;
-import view.Navbar;
-import view.HpUserView;
-import view.EditProductScene;
 
 public class Main extends Application {
 
@@ -15,11 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Create an instance of the LoginView and pass a new LoginController to it
-        //new LoginView(primaryStage).show();
-        //new HpUserView(primaryStage,null).show();
-        // Start the login view
-    	EditProductScene test=new EditProductScene(primaryStage,new User("1", "admin", "admin123", "admin", "industri","0389921", "Male"));
-        test.show();
-    }
+         // Create an instance of the LoginView and pass a new LoginController to it
+         LoginView loginView = new LoginView(primaryStage);
+
+         // Start the login view
+         loginView.show();
+     }
 }
